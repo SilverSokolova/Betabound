b71ebd2a3b47b47b2b70a6728a396248865d8abb
SilverSokolova <xtechresearchcenter2@gmail.com>
2023-10-09 16:49:49 -0500

    upgrade vanilla object icons

--- npcs/sb_outpostcivilian.lua
+++ npcs/sb_outpostcivilian.lua
@@ -4,5 +4,5 @@ local ini = init or function() end
 function init() ini()
   local quests = {config.getParameter("sb_offeredQuests"),config.getParameter("sb_turnInQuests")}
   if quests[1] then npc.setOfferedQuests(quests[1]) end
-  if quests[2] then npc.setTurnInQuests(quests[2]) end --though it'd be cool to just use [1] if [2] if a number, that'd screw over people trying to add quests
+  if quests[2] then npc.setTurnInQuests(quests[2]) end --though it'd be cool to just use [1] if [2] is a number, that'd screw over people trying to add quests since it wouldn't add ours
 end
