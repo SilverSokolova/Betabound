7f33d6932d2aa393b0f2816a6fbc3443701244e6
SilverSokolova <xtechresearchcenter2@gmail.com>
2023-10-10 15:02:49 -0500

    remove esther painting

--- monsters/walkers/sb_speech/monsterspawner.lua
+++ monsters/walkers/sb_speech/monsterspawner.lua
@@ -1,4 +1,10 @@
 function init()
-	local a = config.getParameter("sb_monsterspawner")
-	world.spawnMonster(a[math.random(#a)],entity.position(),{aggressive=config.getParameter("aggressive",true),damageTeam=entity.damageTeam().team,level=monster.level() or world.threatLevel() or 1})
+	local data = config.getParameter("sb_monsterspawner")
+	world.spawnMonster(data[math.random(#data)], entity.position(),
+    {
+      aggressive = config.getParameter("aggressive", true),
+      damageTeam = entity.damageTeam().team,
+      level = monster.level() or world.threatLevel() or 1
+    }
+  )
 end
