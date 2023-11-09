826de76879a593eb18b79ff15c76a91496dccebd
SilverSokolova <xtechresearchcenter2@gmail.com>
2023-11-09 01:31:22 -0500

    whips

--- items/tools/miningtools/sb_beamaxeanimation.lua
+++ items/tools/miningtools/sb_beamaxeanimation.lua
@@ -17,16 +17,16 @@ function fillRadius(radius)
 	localAnimator.addDrawable({image = endImages[1],fullbright=true,position = base},layer)
 	base[1] = math.floor(math.floor(base[1])) base[2] = math.floor(math.floor(base[2]))
 	if radius % 2 == 0 then base = {base[1]+0.4,base[2]+0.4} end
-	if radius == 1 then light({base[1]+0.4,base[2]+0.4},layer) return end
+	if radius == 1 then addLight({base[1]+0.4,base[2]+0.4},layer) return end
 	local t = {}
 	for x = 1, radius do
 		for y = 1, radius do
-			t[#t+1] = {base[1]-(radius/2)+x,base[2]-(radius/2)+y} light(t[#t],layer) --if #t>1 then light(t[#t],layer) end
+			t[#t+1] = {base[1]-(radius/2)+x,base[2]-(radius/2)+y} addLight(t[#t],layer) --if #t>1 then addLight(t[#t],layer) end
 		end
 	end
 end
 
-function light(pos,layer)
+function addLight(pos,layer)
   localAnimator.addDrawable({image = endImages[2],fullbright=true,position = pos},layer)
   localAnimator.addLightSource({position = pos, color = {25.5, 40, 46.75},pointLight=false,beamAmbience=0.00002})
 end
