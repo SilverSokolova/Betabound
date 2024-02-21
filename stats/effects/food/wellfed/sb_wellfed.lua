477d9adb5dae50d75d3fbc7cd38e6aa6425f48b4
SilverSokolova <xtechresearchcenter2@gmail.com>
2024-02-21 14:46:29 -0600

    RC 12

--- stats/effects/food/wellfed/sb_wellfed.lua
+++ stats/effects/food/wellfed/sb_wellfed.lua
@@ -4,9 +4,3 @@ local updat = update or function() end
 function init() ini()
   effect.addStatModifierGroup({{stat = config.getParameter("sb_stat", "maxHealth"), effectiveMultiplier = config.getParameter("sb_amount", 1.1)}})
 end
-
-function update(...) updat(...)
-  if status.uniqueStatusEffectActive("foodpoison") then
-    effect.expire()
-  end
-end
