cf57c87514f456eb2f2c77aef75d7e7d267499c0
SilverSokolova <xtechresearchcenter2@gmail.com>
2024-06-29 01:29:41 -0500

    party popper changes

--- items/active/starbound/partypopper/partypopper.lua
+++ items/active/starbound/partypopper/partypopper.lua
@@ -1,13 +1,14 @@
-require "/scripts/vec2.lua"
+--WONTFIX: You can reuse them by dropping them before they're consumed
 
 function init()
   emotes = config.getParameter("emotes")
-  emoteChance = config.getParameter("emoteChance",2)
-  consumeTimer = config.getParameter("consumeTimer",0.6)
+  emoteChance = config.getParameter("emoteChance", 2)
+  storage.consumeTimer = storage.consumeTimer or config.getParameter("consumeTimer", 0.6)
   fireOffset = config.getParameter("fireOffset")
+  baseFireTime = config.getParameter("fireTime", 1)
   recoil = 0
   recoilRate = 0
-  active, firing = false, false
+  active, firing = false, firing or false
   storage.fireTimer = storage.fireTimer or 0
   animator.setPartTag("muzzleFlash", "variant", "1")
   updateAim()
@@ -17,27 +18,30 @@ function update(dt, fireMode, shiftHeld)
   updateAim()
   storage.fireTimer = math.max(storage.fireTimer - dt, 0)
 
-  if active then
-    recoilRate = 0
-  else
-    recoilRate = math.max(1, recoilRate + (10 * dt))
-  end
+  recoilRate = active and 0 or math.max(1, recoilRate + (10 * dt))
   recoil = math.max(recoil - dt * recoilRate, 0)
 
   if active and storage.fireTimer <= 0 then
-    consumeTimer = math.max(0, consumeTimer - dt)
+    storage.consumeTimer = math.max(0, storage.consumeTimer - dt)
     recoil = recoil + 0.1 -- aimAngle
     if animator.animationState("firing") == "off" then
-      if math.random(emoteChance) == 1 then activeItem.emote(emotes[math.random(1,#emotes)]) end
+      if math.random(emoteChance) == 1 then
+        activeItem.emote(emotes[math.random(#emotes)])
+      end
       animator.setAnimationState("firing", "fire")
     end
     animator.setPartTag("muzzleFlash", "variant", math.random(1, 3))
-    storage.fireTimer = config.getParameter("fireTime", 1.0)
+    storage.fireTimer = baseFireTime
   end
   active = false
 
-  if firing then consumeTimer = math.max(0, consumeTimer - dt)
-    if consumeTimer <= 0 then item.consume(1) consumeTimer = 1 firing = false end
+  if firing then
+    storage.consumeTimer = math.max(0, storage.consumeTimer - dt)
+    if storage.consumeTimer <= 0 then
+      item.consume(1)
+      storage.consumeTimer = 1
+      firing = false
+    end
   end
 end
 
