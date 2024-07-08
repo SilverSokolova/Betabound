138508450e508e95e3c95de402a4fddd2e28597b
SilverSokolova <xtechresearchcenter2@gmail.com>
2024-07-08 17:53:09 -0500

    make paint go into block tab

--- scripts/augments/sb_paint.lua
+++ scripts/augments/sb_paint.lua
@@ -3,7 +3,11 @@ require "/scripts/augments/item.lua"
 function apply(input)
   local output = Item.new(input)
   if root.itemConfig(output.name).config.materialId then
-    output:setInstanceValue("materialHueShift",config.getParameter("sb_paint",0))
-    return output:descriptor(), 1
+    local newHueshift = config.getParameter("sb_paint", 0)
+    local oldHueshift = output:instanceValue("materialHueShift", 1)
+    if newHueshift ~= oldHueshift then
+      output:setInstanceValue("materialHueShift", newHueshift)
+      return output:descriptor(), 1
+    end
   end
 end
