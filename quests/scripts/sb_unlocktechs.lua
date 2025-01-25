ae24b5981cff36d9413b9a1a5370dd1467a3cb01
SilverSokolova <xtechresearchcenter2@gmail.com>
2025-01-25 03:14:16 -0600

    dont radio message while cutscene is playing

--- quests/scripts/sb_unlocktechs.lua
+++ quests/scripts/sb_unlocktechs.lua
@@ -1,11 +1,13 @@
 function init()
   local questData = root.questConfig("sb_techunlocks")
   radioMessages = questData.scriptConfig.radioMessages
+  delay = questData.scriptConfig.cinematicRadioMessageDelay
+  storage.delayTimer = storage.delayTimer or 0
   require(questData.script)
 
   sb_techType()
   local techs = config.getParameter("techs")
-  local unownedTechs = {}
+  unownedTechs = {}
   for i = 1, #techs do
     if not ownsTech(techs[i]) then
       unownedTechs[#unownedTechs + 1] = techs[i]
@@ -13,13 +15,21 @@ function init()
   end
 
   if #unownedTechs ~= 0 then
+    local sb_genderedCinematic = config.getParameter("sb_genderedCinematic")
+    if sb_genderedCinematic then
+      player.playCinematic(string.format(sb_genderedCinematic, player.gender() == "male" and "m" or "f"))
+    end
+  end
+end
+
+function update(dt)
+  if not unownedTechs then
+    quest.complete()
+  end
+
+  storage.delayTimer = storage.delayTimer + dt
+  if unownedTechs and storage.delayTimer > delay then
     sendRadioMessage(unownedTechs)
+    unownedTechs = nil
   end
-
-  local sb_genderedCinematic = config.getParameter("sb_genderedCinematic")
-  if sb_genderedCinematic and #unownedTechs ~= 0 then
-    player.playCinematic(string.format(sb_genderedCinematic, player.gender() == "male" and "m" or "f"))
-  end
-
-  quest.complete()
 end
