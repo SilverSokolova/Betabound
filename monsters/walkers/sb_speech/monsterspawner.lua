f5be0064e6c0ccb98a2c766cb0dde5bbb65b2bfe
SilverSokolova <xtechresearchcenter2@gmail.com>
2025-02-05 23:48:08 -0600

    fix dungeon monsters being friendly

--- monsters/walkers/sb_speech/monsterspawner.lua
+++ monsters/walkers/sb_speech/monsterspawner.lua
@@ -1,11 +1,9 @@
 function init()
   local data = config.getParameter("sb_monsterspawner")
-  local damageTeam = entity.damageTeam()
   world.spawnMonster(data[math.random(#data)], entity.position(),
     {
       aggressive = config.getParameter("aggressive", true),
-      damageTeam = damageTeam.team,
-      damageTeamType = damageTeam.type,
+      damageTeam = entity.damageTeam().team, --Setting the damageTeamType messes things up
       level = monster.level() or world.threatLevel() or 1
     }
   )
