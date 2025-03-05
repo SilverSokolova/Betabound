f52fc7293aab246ca7de4c1ac00cc04547915a3f
SilverSokolova <xtechresearchcenter2@gmail.com>
2025-03-05 04:06:39 -0600

    slight optimizations

--- stats/sb_player_primary.lua
+++ stats/sb_player_primary.lua
@@ -9,30 +9,42 @@ function init() originalInit()
   player = math.betabound_player
   math.betabound_mcontroller = _ENV.mcontroller
   sb_shieldAlpha = {0,0,0,0}
-  sb_lastHunger = math.floor(status.resourcePercentage("food")*100)
-  sb_lastHungerMessage = "d100"
-  sb_hungerBenchmarks = {2,5,10,15,25,50,75,100}
+
   sb_hungerPopups = root.assetJson("/betabound.config:hungerPopups")
+  if sb_hungerPopups then
+    sb_lastHunger = math.floor(status.resourcePercentage("food")*100)
+    sb_lastHungerMessage = "d100"
+    sb_hungerBenchmarks = {2,5,10,15,25,50,75,100}
+  end
 end
 
 function update(dt) originalUpdate(dt)
-  if player and not starExtensions then
+  if not player then
+    player = math.betabound_player
+  end
+
+  --reentry
+  if not starExtensions then
     animator.setAnimationState("sb_flames", not player.isLounging() and not mcontroller.zeroG() and mcontroller.yVelocity() <= -170 and "flames" or "none")
   end
+
+  --shields
   if not status.resourcePositive("sb_shieldStaminaRegenBlockL") then
     status.modifyResourcePercentage("sb_shieldStaminaL", status.stat("shieldStaminaRegen") * dt)
     status.modifyResourcePercentage("sb_perfectBlockLimitL", status.stat("perfectBlockLimitRegen") * dt)
   end
+
   if not status.resourcePositive("sb_shieldStaminaRegenBlockR") then
     status.modifyResourcePercentage("sb_shieldStaminaR", status.stat("shieldStaminaRegen") * dt)
     status.modifyResourcePercentage("sb_perfectBlockLimitR", status.stat("perfectBlockLimitRegen") * dt)
   end
 
+  --hunger
   if sb_hungerPopups then
     local maxHunger = status.resourceMax("food") --grab this in update in case it changes
-    local hunger = math.floor(status.resourcePercentage("food")*maxHunger)
+    local hunger = math.floor(status.resourcePercentage("food") * maxHunger)
     if hunger ~= sb_lastHunger then
-      for i = 1, #sb_hungerBenchmarks-1 do
+      for i = 1, #sb_hungerBenchmarks - 1 do
         if hunger > sb_hungerBenchmarks[i] and hunger < sb_hungerBenchmarks[i+1] then
           local id = (hunger > sb_lastHunger and "u" or "d")..sb_hungerBenchmarks[i+1]
           if sb_lastHungerMessage ~= id then
@@ -42,13 +54,14 @@ function update(dt) originalUpdate(dt)
         end
       end
     end
-    sb_lastHunger = math.floor(status.resourcePercentage("food")*maxHunger)
+    sb_lastHunger = math.floor(status.resourcePercentage("food") * maxHunger)
   end
 end
 
 function applyDamageRequest(damageRequest)
   if (world.getProperty("invinciblePlayers", false) or world.getProperty("nonCombat", false)) then return {} end
-  if not player then player = math.betabound_player end
+
+  --force field tech
   if status.resource("sb_forceFieldStrength") > 0 and status.resourcePositive("energy") and not status.resourceLocked("energy") then --resourcePositive rounds or smth
     local forceFieldStrength = status.resource("sb_forceFieldStrength")
     local maxReduction = math.min(math.min(damageRequest.damage, (status.resource("energy")/2) * forceFieldStrength), 0)
@@ -57,6 +70,7 @@ function applyDamageRequest(damageRequest)
     return originalApplyDamageRequest(damageRequest)
   end
 
+  --shield tech
   if status.resourcePositive("sb_shieldStaminaT") then
     if damageRequest.sourceEntityId == -65536 then --NOTE: this does not block self-inflicted projectile damage such as bombs
       originalApplyDamageRequest(damageRequest)
@@ -65,16 +79,20 @@ function applyDamageRequest(damageRequest)
       return originalApplyDamageRequest(damageRequest)
     end
   end
+
+  --shields
   if damageRequest.hitType ~= "ShieldHit" or damageRequest.sourceEntityId == -65536 then return originalApplyDamageRequest(damageRequest) end
-  local oldDamage = damageRequest.damage
-  damageRequest.damage = damageRequest.damage + root.evalFunction2("protection", damageRequest.damage, status.stat("protection"))/4
+  local baseDamage = damageRequest.damage
+
+  damageRequest.damage = damageRequest.damage + root.evalFunction2("protection", damageRequest.damage, status.stat("protection")) / 4
   if damageRequest.damage <= 0 then return {} end
+
   if status.statPositive("sb_shieldHealthL") then
-    return sb_applyShieldDamage("L",damageRequest)
+    return sb_applyShieldDamage("L", damageRequest)
   elseif status.statPositive("sb_shieldHealthR") then
-    return sb_applyShieldDamage("R",damageRequest)
+    return sb_applyShieldDamage("R", damageRequest)
   else
-    damageRequest.damage = oldDamage
+    damageRequest.damage = baseDamage
     return originalApplyDamageRequest(damageRequest)
   end
 end
@@ -95,7 +113,6 @@ function sb_applyShieldDamage(hand,damageRequest)
 end
 
 function overheadBars()
-  if not player then player = math.betabound_player end
   local bars = originalOverheadBars()
   sb.setLogMap("sb_shields","%s (%s), %s (%s), %s",status.resource("sb_shieldStaminaL"),status.stat("sb_shieldHealthL"),status.resource("sb_shieldStaminaR"),status.stat("sb_shieldHealthR"),status.resource("sb_forceFieldStrength"))
   sb.setLogMap("sb_techtier","%s",player and player.getProperty("sb_techTier","-") or "UNAVAILABLE")
@@ -105,24 +122,24 @@ function overheadBars()
   sb_shieldAlpha[3] = status.resourcePercentage("sb_shieldStaminaT") == 0 and math.max(0,sb_shieldAlpha[3]-15) or 255
 
   if sb_shieldAlpha[1] > 0 or status.statPositive("sb_shieldHealthL") or status.resourcePercentage("sb_shieldStaminaL") < 1 then
-    table.insert(bars, {
+    bars[#bars+1] = {
       icon = "/interface/sb_shieldL.png?multiply=FFFFFF"..string.format("%02X", sb_shieldAlpha[1]),
       percentage = status.resource("sb_shieldStaminaL"),
       color = {30, 121, status.resourcePositive("sb_perfectBlockL") and 255 or 188, sb_shieldAlpha[1]}
-    })
+    }
   end
   if sb_shieldAlpha[2] > 0 or status.statPositive("sb_shieldHealthR") or status.resourcePercentage("sb_shieldStaminaR") < 1 then
-    table.insert(bars, {
+    bars[#bars+1] = {
       icon = "/interface/sb_shieldR.png?multiply=FFFFFF"..string.format("%02X", sb_shieldAlpha[2]),
       percentage = status.resource("sb_shieldStaminaR"),
       color = {status.resourcePositive("sb_perfectBlockR") and 255 or 177, 42, 48, sb_shieldAlpha[2]}
-    })
+    }
   end
   if sb_shieldAlpha[3] > 0 or status.resourcePercentage("sb_shieldStaminaT") > 0 then
-    table.insert(bars, {
+    bars[#bars+1] = {
       percentage = status.resource("sb_shieldStaminaT"),
       color = {106, 225, 255, sb_shieldAlpha[3]}
-    })
+    }
   end
 
   return bars
