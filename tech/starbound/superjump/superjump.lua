46cd44f505e77b9419584b411b7028ff8cfea81f
SilverSokolova <xtechresearchcenter2@gmail.com>
2025-06-07 02:28:39 -0500

    reverse chargejump tech controls

--- tech/starbound/superjump/superjump.lua
+++ tech/starbound/superjump/superjump.lua
@@ -1,3 +1,5 @@
+--TODO: rewrite this completely
+--We need to temporarily supress jumping so the player doesn't initiate a regular jump and cancel their approachYVelocity
 function init()
   superJumpTimer = {0,0}
   superJumpSpeed = config.getParameter("superjumpSpeed")
@@ -9,7 +11,11 @@ end
 
 function update(args)
   local activeMovementAbility = status.statPositive("activeMovementAbilities")
-  if not activeMovementAbility and args.moves["jump"] and mcontroller.onGround() and superJumpTimer[1] <= 0 and superJumpCharge <= superJumpMaxCharge then
+  if not activeMovementAbility and args.moves["up"] and mcontroller.onGround() then
+    mcontroller.controlModifiers({jumpingSuppressed = true})
+  end
+
+  if not activeMovementAbility and args.moves["up"] and mcontroller.onGround() and superJumpTimer[1] <= 0 and superJumpCharge <= superJumpMaxCharge then
     superJumpCharge = superJumpCharge + 0.1
   elseif
     superJumpCharge > 0.1 then superJumpCharge = superJumpCharge - 0.1
@@ -18,8 +24,8 @@ function update(args)
   if not activeMovementAbility then
     animator.setParticleEmitterActive("chargedParticles", superJumpCharge >= 8)
 
-    if superJumpCharge >= 1 and superJumpCharge <= 1.1 and mcontroller.onGround() and args.moves["jump"] then animator.playSound("charge") end
-    if args.moves["jump"] and args.moves["up"] and mcontroller.onGround() and superJumpTimer[1] <= 0 and status.overConsumeResource("energy", config.getParameter("energyUsage")) then
+    if superJumpCharge >= 1 and superJumpCharge <= 1.1 and mcontroller.onGround() and args.moves["up"] then animator.playSound("charge") end
+    if args.moves["up"] and args.moves["jump"] and mcontroller.onGround() and superJumpTimer[1] <= 0 and status.overConsumeResource("energy", config.getParameter("energyUsage")) then
       animator.playSound("jumpSound")
       superJumpTimer = {superJumpTime, superJumpTime*3}
     end
@@ -27,6 +33,8 @@ function update(args)
     animator.setFlipped(mcontroller.facingDirection() < 0)
 
     if superJumpTimer[1] > 0 then
+      
+    mcontroller.controlModifiers({jumpingSuppressed = true})
       mcontroller.controlApproachYVelocity(superJumpSpeed+(superJumpCharge*(superJumpCharge*3)), superJumpControlForce+(superJumpCharge*superJumpCharge*3) + world.gravity(mcontroller.position()))
       superJumpTimer[1] = superJumpTimer[1] - args.dt
     else
