35607057551a8c6eaeeeb6554181a4194a72d357
SilverSokolova <xtechresearchcenter2@gmail.com>
2025-11-06 02:20:22 -0600

    kill stuck event dropships

---
+++ vehicles/dropship/sb_eventdropship.lua
@@ -0,0 +1,16 @@
+require "/vehicles/dropship/eventdropship.lua"
+
+local originalUpdate = update or function(...) end
+
+function update(dt); originalUpdate(dt)
+  if mcontroller.isColliding() and not mcontroller.isNullColliding() then
+    storage.health = storage.health - math.random(5)
+    if storage.health <= 0 and not sb_spawnedNpc then
+      sb_spawnedNpc = true
+      local position = entity.position()
+      position[1] = position[1] + (2 * config.getParameter("flyDir"))
+      position[2] = position[2] + 1.75
+      world.spawnNpc(position, "human", "sb_beamoutanddie", 0)
+    end
+  end
+end
