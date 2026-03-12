df1869c91f66b5c3638142f01623477b9ac962e8
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-03-11 21:36:57 -0500

    brain extractor indicator
    Creatures hit with the Brain Extractor show an image indicating immunity if they cannot be brain extracted

--- stats/sb_effects/brainextractor.lua
+++ stats/sb_effects/brainextractor.lua
@@ -2,8 +2,19 @@ require "/scripts/util.lua"
 
 function init()
   brain = config.getParameter("brainMap")[world.entityTypeName(entity.id())]
+
+  if not brain then
+    update = function()
+      local position = entity.position()
+      position[1] = position[1] + -2
+      position[2] = position[2] + 0.75
+      spawnNoEffectParticle("/interface/statuses/sb_brainextractorblock.png", position)
+      effect.expire()
+    end
+    return 
+  end
+
   if type(brain) == "number" then brain = config.getParameter("brainPool") end
-  if not brain then effect.expire() return end
   effect.setParentDirectives(config.getParameter("color"))
   effect.addStatModifierGroup({{stat = "healthRegen", effectiveMultiplier = 0}})
 end
@@ -16,4 +27,32 @@ function uninit()
     end
     effect.expire()
   end
+end
+
+function spawnNoEffectParticle(texture, position)
+  world.spawnProjectile("invisibleprojectile", position, entity.id(), {0, 0}, false,
+      {
+        damageType = "nodamage",
+        timeToLive = 0,
+        piercing = true,
+        speed = 0,
+        power = 0,
+        actionOnReap = {{
+          action = "particle",
+          specification = {
+            type = "textured",
+            image = texture,
+            fullbright = true,
+            size = 1,
+            layer = "front",
+            timeToLive = 0.5,
+            destructionAction = "fade",
+            destructionTime = 0.5,
+            angularVelocity = 250,
+            initialVelocity = {-2.2, 2.1}
+          }
+        }
+      }
+    }
+  )
 end
