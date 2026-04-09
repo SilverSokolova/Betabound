f1002fbb04fe47983562bd85c8580068109d9094
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-04-09 04:44:48 -0500

    Hotfix parity for MM Upgrade Tree fix

--- interface/scripted/mmupgrade/sb_mmupgradegui.lua
+++ interface/scripted/mmupgrade/sb_mmupgradegui.lua
@@ -14,22 +14,9 @@ function sb_hideGui()
   for i = 0, 6 do widget.setImage("imgSbLine"..i,"/assetmissing.png") end
 end
 
-function init()
-  --Did a mod which loaded after us try to let us know the UI was replaced?
-  --This prevents the "OpenUI > Betabound > MM Upgrade Tree > OpenUI" loop
-  --Untested because I decided on a different method
-  --[[
-  if config.getParameter("replaced") then
-    player.say("!")
-    sb_hideGui()
-    updateGui = originalUpdateGui
-    performUpdate = originalPerformUpdate
-    return
-  end]]
+function init(); originalInit()
 
-  originalInit()
-
-  --Must be hidden by default, in case our script isn't ran
+  --Must be hidden by default, in case our script isn't in the config
   widget.setVisible("btnSbBeamaxe", true)
   widget.setVisible("imgSbLine5", true)
 
@@ -38,10 +25,10 @@ function init()
     or betterResetTools) --moremmupgrades
   then
     sb_moveGui()
-  elseif
-    config.getParameter("upgradeBox") --An overhaul of Matter Manipulator Upgrades
-  then
-    sb_hideGui()
+--  elseif
+--    config.getParameter("upgradeBox") --An overhaul of Matter Manipulator Upgrades
+--  then
+--    sb_hideGui()
   end
   self.sb_defaultDescription = self.defaultDescription or ""
   sb_statusProperties = {}
