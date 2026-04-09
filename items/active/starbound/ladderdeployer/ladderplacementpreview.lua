ca8b36618a4fad1a9fd3cb91d090d1f3d933f025
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-04-09 04:14:25 -0500

    Ladder Deployer changes
        • Hold SHIFT and press W or S to make the ladder longer or shorter, up to 25* high
        • The ladder deployer remembers how many ladders you set it to
        • With OpenStarbound, the ladder deployer will numerically show how many ladders it is set to
        • Admin characters are assumed to have medieval ladders* if they do not have any ladders in their inventory
        • (* can be changed with mods or console commands)

--- items/active/starbound/ladderdeployer/ladderplacementpreview.lua
+++ items/active/starbound/ladderdeployer/ladderplacementpreview.lua
@@ -5,19 +5,22 @@ function init()
   floor = math.floor
   clearDrawables = localAnimator.clearDrawables
   addDrawable = localAnimator.addDrawable
-  ladderCount = animationConfig.animationParameter("ladderCount")-2
 end
 
 function update()
+  clearDrawables()
+  ladderCount = animationConfig.animationParameter("ladderCount") - 2
+
   previewImage = animationConfig.animationParameter("previewImage")
   previewPosition = animationConfig.animationParameter("previewPosition")
 
-  clearDrawables()
   for i = 1, #previewPosition do
     previewPosition[i] = floor(previewPosition[i])
   end
-  previewPosition[2] = previewPosition[2]+0.5
-  if previewPosition then
+
+  previewPosition[2] = previewPosition[2] + 0.5
+
+  if previewPosition then --Okay, but what about earlier??
     directives = animationConfig.animationParameter("previewValid") and "?fade=5F50;0.25?border=2;6F67;0000" or "?fade=F550;0.25?border=2;F667;0000"
     addLadder(":bottom")
     for i = 1, ladderCount do
@@ -29,7 +32,7 @@ end
 
 function addLadder(frame)
   addDrawable({
-    image = previewImage..frame..directives,
+    image = previewImage .. frame .. directives,
     position = previewPosition,
     fullbright = true
   })
