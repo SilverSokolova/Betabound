bff57ee9d3b04632738e5b0d090db4e8fa0f1732
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-05-03 22:32:30 -0500

    another hotfix

--- quests/scripts/story/sb_techunlocks.lua
+++ quests/scripts/story/sb_techunlocks.lua
@@ -3,7 +3,7 @@ require("/scripts/sb_assetmissing.lua")
 require("/scripts/player/sb_hasTech.lua")
 
 --Update instead of init because entity messages
-function init(); sb_techType()
+function update(); sb_techType()
   if not player.getProperty("sb_enabledSuitTechs") then return end --Skip if the player hasn't been setup or 36-37 versioning ran late
 
   skipMessage = config.getParameter("skipTechUnlockMessages", sb_storyDisablerInstalled())
@@ -30,7 +30,7 @@ function unlockTech(tier)
   local techs = techTiers[tier]
   local unownedTechs = {}
   for i = 1, #techs do
-    if root.hasTech(techs[i]) and not sb_isTechAvailable(techs[i]) and not sb_isTechEnabled(techs[i]) then
+    if root.hasTech(techs[i]) and not sb_isTechAvailableOrEnabled(techs[i]) then
       unownedTechs[#unownedTechs + 1] = techs[i]
 
       if root.techType(techs[i]) == "Suit" then
