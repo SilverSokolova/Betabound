f7365ec7698d1d6b56fb7ebfac1cfa1290d22b80
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-05-28 02:25:11 -0500

    Fix harmless tech unlock script error

--- quests/scripts/sb_unlocktechs.lua
+++ quests/scripts/sb_unlocktechs.lua
@@ -1,9 +1,13 @@
+require "/scripts/util.lua"
+require("/scripts/sb_assetmissing.lua")
+require("/scripts/player/sb_hasTech.lua")
+
 function init()
   local questData = root.questConfig("sb_techunlocks")
   radioMessages = questData.scriptConfig.radioMessages
   delay = questData.scriptConfig.cinematicRadioMessageDelay
   storage.delayTimer = storage.delayTimer or 0
-  require(questData.script)
+--require(questData.script) --commented out to prevent "quests is nil" issue. we only did this for one function anyway
 
   sb_techType()
   local techs = config.getParameter("techs")
@@ -18,7 +22,7 @@ function init()
   if #unownedTechs ~= 0 then
     local sb_genderedCinematic = config.getParameter("sb_genderedCinematic")
     if sb_genderedCinematic then
-      player.playCinematic(string.format(sb_genderedCinematic, player.gender() == "male" and "m" or "f"))
+      player.playCinematic(string.format(sb_genderedCinematic, player.gender() == "male" and "m" or "f")) --TODO: rename the cinematics to 'male' and 'female'?
     end
   end
 end
@@ -32,5 +36,25 @@ function update(dt)
   if unownedTechs and storage.delayTimer > delay then
     sendRadioMessage(unownedTechs)
     unownedTechs = nil
+  end
+end
+
+--Identical to the one in techunlocks
+function sendRadioMessage(techs)
+  local radioMessage = root.assetJson(config.getParameter("radioMessages", radioMessages)[#techs == 1 and 1 or 2]).text --getParameter isn't returning the default for some reason
+  local formattedTechList = ""
+
+  for i = 1, #techs do
+    formattedTechList = formattedTechList..root.techConfig(techs[i]).shortDescription..(i ~= #techs and ", " or ".")
+  end
+
+  if #techs > 0 then
+    player.radioMessage(
+      {
+        messageId = sb.makeUuid(),
+        unique = false,
+        text = string.format(radioMessage, formattedTechList)
+      }
+    )
   end
 end
