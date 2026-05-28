543fd84c764be23bba38a22f96730329e339e9fa
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-05-28 01:53:52 -0500

    pp cleanup
        • Removed Betabound's fix for the Ceremonial Hunting Cavern's Figure Cave Art spawning issue, as Patch Project has a different fix
        • Removed Betabound's fix for the Swarpion being unable to scale blocks, as Patch Project has its own fix
        • Removed Betabound's fix for Avian Village merchants selling items based on stall type, as it has been given to Patch Project. Betabound is keeping its addition of racial weapons being sold, and rare cosmetic items being sold
        • Removed Betabound's restoration for the Frozen Bow's perfect shot sound effect, as it has been given to Patch Project.

--- quests/scripts/story/sb_techunlocks.lua
+++ quests/scripts/story/sb_techunlocks.lua
@@ -7,17 +7,10 @@ function update(); sb_techType()
   if not player.getProperty("sb_enabledSuitTechs") then return end --Skip if the player hasn't been setup or 36-37 versioning ran late
 
   skipMessage = config.getParameter("skipTechUnlockMessages", sb_storyDisablerInstalled())
-  local quests = config.getParameter("quests")
+  local quests = config.getParameter("quests", {})
   for i = 1, #quests do
     if player.hasCompletedQuest(quests[i]) then unlockTech(i) end
   end
-
-  message.setHandler("objectScanned", function(_, a, _, id)
-    if a == false then return end
-    if id == nil then return end
-    local objectName = world.getObjectParameter(id, "sb_objectName")
-    if objectName then player.addScannedObject(objectName) end
-  end)
 
   update = nil
 end
@@ -46,6 +39,7 @@ function unlockTech(tier)
   end
 end
 
+--Identical to the one in unlocktechs. TODO: rename and do what you need to for both scripts
 function sendRadioMessage(techs)
   local radioMessage = root.assetJson(config.getParameter("radioMessages", radioMessages)[#techs == 1 and 1 or 2]).text --getParameter isn't returning the default for some reason
   local formattedTechList = ""
