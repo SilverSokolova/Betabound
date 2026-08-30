e1247a195e81e71a703e717ad8069df6a8d70241
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-08-30 02:01:13 -0500

    move weapon/ability mod items

--- items/buildscripts/starbound/buildabilitymod.lua
+++ items/buildscripts/starbound/buildabilitymod.lua
@@ -1,33 +1,41 @@
 require("/scripts/sb_assetmissing.lua")
+
 function build(directory, config, parameters)
-  abilities = root.assetJson("/sb_abilitymods.config")
+  abilities = root.assetJson("/items/augments/sb_abilitymod/abilitymods.config")
   weaponNames = abilities.weaponNames
   abilities = abilities.abilities
+
   abilityList = config.abilityList or parameters.abilityList
   parameters.ability = parameters.ability or randomAbility()
   config = sb.jsonMerge(config, getAbilityConfig(parameters.ability))
+  abilityData = root.assetJson(root.assetJson("/items/buildscripts/weaponabilities.config")[parameters.ability]).ability
+
+  local iconDirectory = directory .. "icons/"
   config.inventoryIcon = jarray()
   table.insert(config.inventoryIcon, {image = "/items/generated/sb_mod.png"})
-  --decided against `config.icon and (config.icon..".png") in order to support directives
-  local icon = config.icon
-  if icon then
-    if icon:sub(0, 1) ~= "/" then icon = directory..icon end
+  local icon = config.icon --no concatenation, to support directives
+  if icon and icon:sub(0, 1) ~= "/" then
+    icon = iconDirectory .. icon
   end
-  table.insert(config.inventoryIcon, icon and {image = icon} or {image = sb_assetmissing(directory..parameters.ability..".png", "sb_default.png")})
-  abilityData = root.assetJson(root.assetJson("/items/buildscripts/weaponabilities.config")[parameters.ability]).ability
+  table.insert(config.inventoryIcon, icon and {image = icon} or {image = sb_assetmissing(iconDirectory .. parameters.ability .. ".png", iconDirectory .. "sb_default.png")})
 
   local elementalConfig = abilityData.elementalConfig
   if elementalConfig then
-    if config.rarity == "common" then config.rarity = "uncommon" end
+    if config.rarity == "common" then
+      config.rarity = "uncommon"
+    end
+
     local acceptedElements = {}
     for k, v in pairs(elementalConfig) do
       for _, _ in pairs(v) do --we have to do this because there's no table.size and one *elemental* vanilla ability has a blank elemental config for *physical*
-        acceptedElements[#acceptedElements+1] = k
+        acceptedElements[#acceptedElements + 1] = k
         break
       end
     end
+
     config.acceptedElements = acceptedElements
   end
+
   config.shortdescription = string.gsub((config.rarity ~= "common" and "^yellow;" or "") .. (abilityData.name or "unknown") .. "^reset;", "<elementalName>", config.elementalNameDescription) --"unknown" is same as vanilla weapon buildscript
   config.description = string.format(config.description, config.slotNames[(config.slot and config.slot or "alt")] or "^yellow;???")
 
@@ -41,15 +49,19 @@ function build(directory, config, parameters)
   if parameters.level then
     parameters.level = nil
   end
+
   if parameters.abilityList then
     parameters.abilityList = nil
   end
+
   return config, parameters
 end
 
 function getAbilityConfig(ability)
   for k, _ in pairs(abilities) do
-    if k == ability then return abilities[k] end
+    if k == ability then
+      return abilities[k]
+    end
   end
 end
 
@@ -57,7 +69,7 @@ function randomAbility()
   if not abilityList then
     abilityList = {}
     for k, _ in pairs(abilities) do
-      abilityList[#abilityList+1] = k
+      abilityList[#abilityList + 1] = k
     end
   end
   return abilityList[math.random(#abilityList)]
--- items/buildscripts/starbound/buildelementmod.lua
+++ items/buildscripts/starbound/buildelementmod.lua
@@ -1,21 +1,24 @@
 require("/scripts/sb_assetmissing.lua")
 function build(directory, config, parameters)
-  elementName = root.assetJson("/sb_elementmods.config")
-  parameters.element = parameters.element or randomElement(parameters.elementList or config.elementList)
+  parameters.element = parameters.element or randomFromList(parameters.elementList or config.elementList)
+  elementName = root.assetJson("/items/augments/sb_elementmod/elementmods.config")[parameters.element]
+  config.shortdescription = string.format(config.shortdescription, string.gsub(elementName or parameters.element, "^%l", string.upper))
+
   config.inventoryIcon = jarray()
   table.insert(config.inventoryIcon, {image = "/items/generated/sb_mod.png"})
-  table.insert(config.inventoryIcon, {image = sb_assetmissing(directory..parameters.element..".png")})
-  config.shortdescription = string.format(config.shortdescription, string.gsub(elementName[parameters.element] or parameters.element, "^%l", string.upper))
+  table.insert(config.inventoryIcon, {image = sb_assetmissing(directory .. "icons/" .. parameters.element .. ".png")})
+
 
   if parameters.level then
     parameters.level = nil
   end
+
   if parameters.elementList then
     parameters.elementList = nil
   end
   return config, parameters
 end
 
-function randomElement(elementList)
-  return elementList[math.random(#elementList)]
+function randomFromList(list)
+  return list[math.random(#list)]
 end
