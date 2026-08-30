2f057612ffbb7b13f691011dc4f1a22529ff68a0
SilverSokolova <xtechresearchcenter2@gmail.com>
2026-08-30 02:22:23 -0500

    Account for obscure scenario

--- objects/outpost/treasuredtrophies/sb_treasuredtrophies.lua
+++ objects/outpost/treasuredtrophies/sb_treasuredtrophies.lua
@@ -1,23 +1,19 @@
+--Silver Sokolova
 local originalInit = init or function() end
 
 function init(); originalInit()
   local i = config.getParameter("interactData")
   local r = config.getParameter("sb_treasuredtrophies")
+  local newR = {}
 
   for n = 1, #r do
     if world.universeFlagSet(r[n]) then
-      i.filter[#i.filter+1] = "sb_treasuredtrophies_"..r[n]
+      i.filter[#i.filter + 1] = "sb_treasuredtrophies_"..r[n]
+    else
+      newR[#newR + 1] = r[n]
     end
   end
 
-  if not sb_storyDisablerInstalled then
-    require("/scripts/sb_assetmissing.lua")
-  end
-
-  if sb_storyDisablerInstalled() then
-    i.filter[#i.filter + 1] = "sb_treasuredtrophies_beamaxe2"
-  end
-
   object.setConfigParameter("interactData", i)
-  object.setConfigParameter("sb_treasuredtrophies", {}) --TODO: Don't just clear it all, check for what we've already added! We need to do this so we don't infinitely add things and cause that one issue that crashes the game when a groundfirebomb is thrown.
+  object.setConfigParameter("sb_treasuredtrophies", newR) --We need to do this so we don't infinitely add things and cause that one issue that crashes the game when a groundfirebomb is thrown.
 end
